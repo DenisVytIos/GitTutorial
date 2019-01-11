1554582d795d9a7ee168b0fed4251a4e5aaf6175
Denis <>
2019-01-11 12:37:11 +0200

    Add a button

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Denis on 11.01.2019.
+//  Copyright © 2019 Denis Vitrishko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("hello word!")
+        super.viewDidLoad()
+       
+    }
+
+   
+
+
+}
+
